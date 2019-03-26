3257ad6e7c15f05cfb0af83e1311f3268aa139bd
Edson Costa <edsoncosta.jr@hotmail.com>
2019-03-26 17:01:37 -0300

    Validação de horários para cadastrar uma sessão está correta

---
+++ src/test/java/ingresso/GerenciadorDeSessaoTest.java
@@ -0,0 +1,70 @@
+package ingresso;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.taglibs.standard.lang.jstl.MinusOperator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessão;
+
+public class GerenciadorDeSessaoTest {
+	
+	private Filme rogueOne;
+	private Sala sala3D;
+	private Sessao sessaoDasDez;
+	private Sessao sessaoDasTreze;
+	private Sessao sessaoDasDezoito;
+	
+	
+	@Before
+	public void preparaSessoes() {
+		
+		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(120), "Sci-fi");
+		this.sala3D = new Sala("Sala 3D");
+		
+		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
+		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), rogueOne, sala3D);
+		this.sessaoDasDezoito = new Sessao(LocalTime.parse("18:00:00"), rogueOne, sala3D);
+				
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessaoMesmoHorario() {
+		
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		GerenciadorDeSessão gerenciador = new GerenciadorDeSessão(sessoes);
+		Assert.assertFalse(gerenciador.cabe(sessaoDasDez));
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessoesTerminandoDentroDoHorarioDeUmaSessaoJaExistente() {
+		
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		Sessao sessao = new Sessao(sessaoDasDez.getHorario().minusHours(1), rogueOne, sala3D);
+		GerenciadorDeSessão gerenciador = new GerenciadorDeSessão(sessoes);
+		Assert.assertFalse(gerenciador.cabe(sessao));
+	}
+
+	@Test
+	public void garanteQueNaoDevePermitirSessoesIniciandoDentroDoHorarioDeUmaSessaoJaExistente() {
+		List<Sessao> sessoesDaSala = Arrays.asList(sessaoDasDez);
+		GerenciadorDeSessão gerenciador = new GerenciadorDeSessão(sessoesDaSala);
+		Sessao sessao = new Sessao(sessaoDasDez.getHorario().plusHours(1), rogueOne, sala3D);
+		Assert.assertFalse(gerenciador.cabe(sessao));
+	}
+	
+	@Test
+	public void garanteQueDevePermitirUmaInsercaoEntreOsDoisFilmes() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez, sessaoDasDezoito);
+		GerenciadorDeSessão gerenciador = new GerenciadorDeSessão(sessoes);
+		Assert.assertTrue(gerenciador.cabe(sessaoDasTreze));
+	}
+}
