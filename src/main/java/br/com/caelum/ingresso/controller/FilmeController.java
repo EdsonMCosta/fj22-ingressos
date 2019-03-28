866315767b18b8ab26d323a79b95e6416de91c1c
Edson Costa <edsoncosta.jr@hotmail.com>
2019-03-28 11:14:48 -0300

    Criando Lógica para seleção de lugares

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -102,8 +102,8 @@ public class FilmeController {
     	Filme filme = filmeDao.findOne(id);
     	List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
     	
-    	Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
-    	System.out.println(detalhesDoFilme.isPresent());
+    	Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme, DetalhesDoFilme.class);
+    	
     	modelAndView.addObject("sessoes", sessoes);
     	modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
     	    	
